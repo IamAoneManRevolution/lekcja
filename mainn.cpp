492b60991a5928b1c738b561a8cb75b84c34d6dc
Michał Daniel Domarecki <m.domarecki@live.com>
2024-09-28 12:48:15 +0200

    new file

---
+++ mainn.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+//komentarz
+//testowy komentarz na branchu develop
+int main()
+{
+    //:D
+    cout << " Hello world " << " Conflict " << endl;
+    //test3
+    //test
+    
+    return 0;
+}
